23070a55c783090361f0ddf9fdeee33e496e747e
gilmarinoj <gilmarinoj@gmail.com>
2024-12-09 07:57:21 -0400

    second commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+    
+
+<x-app-layout>
+
+    <div class="max-w-4xl mx-auto px-4">
+        <h1>Bienvenido a la pagina principal</h1>
+    </div>
+
+    <x-alert2 type="info" class="mb-4">
+        <x-slot name="title">
+            Hello
+        </x-slot>
+
+        I'm testing this feature.
+    </x-alert2>
+
+    <p>Hola Mundo</p>
+
+</x-app-layout>
+
+
