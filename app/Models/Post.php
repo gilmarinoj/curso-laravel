b697466bd243df40449a771eceaa9a980850a980
gilmarinoj <gilmarinoj@gmail.com>
2024-12-09 13:06:27 -0400

    refactorized

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+<<<<<<< HEAD
     protected $table = 'posts';
+=======
+    protected $table = 'posts';  
+>>>>>>> daf56b6 (third commit)
 }
